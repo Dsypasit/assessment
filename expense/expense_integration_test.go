1405521919d012ae58439cd6cb9c95d90279303a
dsypasit <ong.pasit@gmail.com>
2022-12-20 19:46:50 +0700

    add it test for update expense

--- expense/expense_integration_test.go
+++ expense/expense_integration_test.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -153,3 +154,65 @@ func TestGetExpenseByID(t *testing.T) {
 
 }
 
+func TestUpdateExpense(t *testing.T) {
+	body := bytes.NewBufferString(`
+	{
+	"title": "strawberry smoothie 2",
+	"amount": 7,
+	"note": "night market promotion discount 100 bath", 
+	"tags": ["food"]
+}
+	`)
+
+	updateBody := bytes.NewBufferString(`
+{
+    "title": "apple smoothie",
+    "amount": 89,
+    "note": "no discount",
+    "tags": ["beverage"]
+}
+	`)
+
+	eh := echo.New()
+	InitDB()
+	go func(e *echo.Echo) {
+		CreateRoute(e)
+		e.Start(":5000")
+	}(eh)
+
+	for {
+		conn, err := net.DialTimeout("tcp", "localhost:5000", 30*time.Second)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if conn != nil {
+			conn.Close()
+			break
+		}
+	}
+
+	var expected Expense
+	res := request(http.MethodPost, url("expenses"), body)
+	err := res.Decode(&expected)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+
+	var result Expense
+	res = request(http.MethodPut, url("expenses", strconv.Itoa(expected.ID)), updateBody)
+	err = res.Decode(&result)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	assert.Equal(t, expected.ID, result.ID)
+	assert.Equal(t, "apple smoothie", result.Title)
+	assert.Equal(t, 89.0, result.Amount)
+	assert.Equal(t, "no discount", result.Note)
+	assert.ElementsMatch(t, []string{"beverage"}, result.Tags)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = eh.Shutdown(ctx)
+	assert.NoError(t, err)
+
+}
