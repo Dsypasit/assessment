e95085210c0cc2906a51ad143202151791a718dc
dsypasit <ong.pasit@gmail.com>
2022-12-20 17:04:32 +0700

    use echo library

--- server.go
+++ server.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Dsypasit/assessment/expense"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	expense.InitDB()
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
