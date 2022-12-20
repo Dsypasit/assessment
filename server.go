02d6a712cbe2d6682b5e49acd617ea4e5a234366
dsypasit <ong.pasit@gmail.com>
2022-12-20 21:02:25 +0700

    add middleware

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/Dsypasit/assessment/expense"
@@ -10,6 +12,17 @@ import (
 func main() {
 	expense.InitDB()
 	e := echo.New()
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			auth := c.Request().Header.Values("Authorization")[0]
+			if auth != "November 10, 2009" {
+				return c.JSON(http.StatusUnauthorized, "Unauthorized")
+			}
+			fmt.Println("from middle ware")
+			return next(c)
+		}
+	})
 
 	expense.CreateRoute(e)
 
