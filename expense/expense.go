c984750afabc8e1afd1b0ae4d8e3ce45ff8427f9
dsypasit <ong.pasit@gmail.com>
2022-12-20 17:07:12 +0700

    add expense sruct

---
+++ expense/expense.go
@@ -0,0 +1,12 @@
+package expense
+
+import "github.com/labstack/echo/v4"
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+	Title  string   `json:"title"`
+}
+
