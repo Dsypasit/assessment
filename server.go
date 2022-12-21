a5db202cecdccacbca8bb91142f13ae1a0999f41
dsypasit <ong.pasit@gmail.com>
2022-12-21 08:42:15 +0700

    add graceful shutdown

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Dsypasit/assessment/expense"
 	"github.com/labstack/echo/v4"
@@ -15,5 +20,22 @@ func main() {
 
 	expense.CreateRoute(e)
 
-	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	log.Println("Server up by port 2565")
+	go func() {
+		if err := e.Start(":2565"); err != nil {
+			e.Logger.Fatal("shutdown server")
+		}
+	}()
+
+	<-shutdown
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
